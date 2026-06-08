c71a609bd6a1d60069d8eb2a1ab98223adb74adc
shoira0594-bit <shoira0594@gmail.com>
2026-06-08 09:46:59 +0500

    Update laboratoriya.py

--- laboratoriya.py
+++ laboratoriya.py
@@ -16,70 +16,70 @@ questions = [
     {
         "id": 1, 
         "text": "1. Qattiq jismning solishtirma issiqlik sig’imini aniqlash laboratoriya ishida tashqi muhit bilan issiqlik almashinuvini kamaytirish uchun qaysi asbobdan foydalaniladi?", 
-        "audio": "audio_files/savol_1.mp3",
+        "audio": "savol_1.mp3",
         "options": ["A) Voltmetr", "B) Kalorimetr", "C) Termometr", "D) Ampermetr"],
         "correct": "B) Kalorimetr"
     },
     {
         "id": 2, 
         "text": "2. Solishtirma issiqlik sig’imini hisoblash formulasida jism qabul qilgan yoki ajratgan issiqlik miqdori qaysi bosh harf bilan belgilanadi?", 
-        "audio": "audio_files/savol_2.mp3",
+        "audio": "savol_2.mp3",
         "options": ["A) m", "B) T", "C) c", "D) Q"],
         "correct": "D) Q"
     },
     {
         "id": 3, 
         "text": "3. Xalqaro birliklar sistemasida moddaning solishtirma issiqlik sig’imi birligi qaysi javobda to’g’ri so’zlar bilan ko’rsatilgan?", 
-        "audio": "audio_files/savol_3.mp3",
+        "audio": "savol_3.mp3",
         "options": ["A) Joul taqsim kilogramm ko'paytirilgan Kelvin", "B) Nyuton taqsim metr", "C) Watt taqsim sekund", "D) Kaloriya taqsim gradus"],
         "correct": "A) Joul taqsim kilogramm ko'paytirilgan Kelvin"
     },
     {
         "id": 4, 
         "text": "4. Laboratoriya ishini bajarishda kalorimetr ichidagi suvga qizdirilgan qattiq jism tashlanganda qanday jarayon yuzaga keladi?", 
-        "audio": "audio_files/savol_4.mp3",
+        "audio": "savol_4.mp3",
         "options": ["A) Mexanik to'qnashuv", "B) Issiqlik almashinuvi (balansi)", "C) Kimyoviy reaksiya", "D) Modda parchalanishi"],
         "correct": "B) Issiqlik almashinuvi (balansi)"
     },
     {
         "id": 5, 
         "text": "5. Laboratoriya ishida jismning massasini aniqlash uchun qaysi o’lchov asbobidan foydalaniladi?", 
-        "audio": "audio_files/savol_5.mp3",
+        "audio": "savol_5.mp3",
         "options": ["A) Menzurka", "B) Laboratoriya tarozi", "C) Lineyka", "D) Sekundomer"],
         "correct": "B) Laboratoriya tarozi"
     },
     {
         "id": 6, 
         "text": "6. Laboratoriya ishida tajriba xatoligini kamaytirish va aniqroq natija olish uchun, qizdirilgan jismni qaynoq suvdan olgach kalorimetrga qanday o’tkazish kerak?", 
-        "audio": "audio_files/savol_6.mp3",
+        "audio": "savol_6.mp3",
         "options": ["A) Sekin, 5 daqiqa kutib", "B) Iloji boricha tez va chaqqonlik bilan", "C) Suvini yaxshilab quritibkeyin", "D) Muzlatib keyin"],
         "correct": "B) Iloji boricha tez va chaqqonlik bilan"
     },
     {
         "id": 7, 
         "text": "7. Menzurka yordamida suvning qaysi kattaligi o’lchanadi va keyinchalik zichlik orqali uning massasi hisoblanadi?", 
-        "audio": "audio_files/savol_7.mp3",
+        "audio": "savol_7.mp3",
         "options": ["A) Temperaturasi", "B) Massasi", "C) Hajmi", "D) Zichligi"],
         "correct": "C) Hajmi"
     },
     {
         "id": 8, 
         "text": "8. Issiqlik balansi tenglamasiga ko’ra, yopiq sistemada issiq jism bergan issiqlik miqdori nimaga teng bo’ladi?", 
-        "audio": "audio_files/savol_8.mp3",
+        "audio": "savol_8.mp3",
         "options": ["A) Sovuq jism olgan issiqlik miqdoriga", "B) Atrofga yo'qolgan energiyaga", "C) Jismning ichki energiyasiga", "D) Nolga"],
         "correct": "A) Sovuq jism olgan issiqlik miqdoriga"
     },
     {
         "id": 9, 
         "text": "9. Moddaning solishtirma issiqlik sig’imi deb nimaga aytiladi?", 
-        "audio": "audio_files/savol_9.mp3",
+        "audio": "savol_9.mp3",
         "options": ["A) 1 kg moddani 1 Kelvinga qizdirish uchun kerak bo'lgan issiqlik miqdoriga", "B) Jismni eritish uchun ketadigan energiyaga", "C) Suyuqlikning qaynash temperaturasiga", "D) Moddaning og'irligiga"],
         "correct": "A) 1 kg moddani 1 Kelvinga qizdirish uchun kerak bo'lgan issiqlik miqdoriga"
     },
     {
         "id": 10, 
         "text": "10. Kalorimetr ichidagi suvga tashlangan issiq jism o’rtasida issiqlik almashinuvi jarayoni qachongacha davom etadi?", 
-        "audio": "audio_files/savol_10.mp3",
+        "audio": "savol_10.mp3",
         "options": ["A) Suv qaynab ketguncha", "B) Jism butunlay erib ketguncha", "C) Tizimda issiqlik muvozanati (temperaturalar tenglashguncha) qaror topguncha", "D) 10 daqiqa o'tguncha"],
         "correct": "C) Tizimda issiqlik muvozanati (temperaturalar tenglashguncha) qaror topguncha"
     }
